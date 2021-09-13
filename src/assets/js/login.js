31ab03d6efc6da7c9ee05912a37fc2f5249ad1d5
lizeth9797 <karina.ortiz@fintecheando.mx>
2021-09-13 01:04:44 -0500

    se agrega funcionalidad del login, al validar las credenciales, cambia a la pagina de DASHBOARD. Al seleccionar registrarme, cambia a la pagina REGISTER

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -1,1 +1,27 @@
 import '../scss/login.scss'
+//import '../../dashboard'
+
+//Login 
+const email=document.getElementById('email')
+const password=document.getElementById('password')
+const login=document.getElementById('login')
+const register=document.getElementById('register')
+login.addEventListener('click',signIn)
+register.addEventListener('click',signUp)
+
+function signIn(){
+    event.preventDefault();
+    const emailValue= email.value
+    const passValue= password.value
+    if(emailValue=="admin@admin.com" && passValue=="password")
+    {
+        location.replace("../../dashboard.html")
+    }else{
+        alert("El usuario y/o contraseña son incorrectos. Inténtelo de nuevo")
+    }
+}
+
+function signUp(){
+    event.preventDefault();
+    location.replace("../../register.html")
+}
