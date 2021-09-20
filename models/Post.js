4c856391f4dca51284173006876f2c2ee302a143
Luu Luu <luu7755@gmail.com>
2021-09-19 21:39:56 -0500

    se añade editar post, filtro por usuario y filtro por tema

--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,7 @@ const PostSchema = new mongoose.Schema({
 
   title: {type: String, require: true}, 
   description: {type: String, require: true}, // Javi recomendó que la longitud se controle en el Frontend
-  topic: {type: String, require: true, enum: ['JavaScript', 'Backend', 'Webpack', 'HTML']}, // Definir si es libre o no
+  topic: {type: String, require: true, enum: ['JavaScript', 'Backend', 'Webpack', 'HTML']}, // Definir cuáles serán los topics permitidos
   author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
 
 }, {timestamps: true, collection: 'posts'})
